481ba8cadd91ec11d559f8796f0bc8be12c22456
Gustavo Sampaio <gustavo.sampaio@zalando.de>
2021-10-07 21:26:02 +0200

    Improving db connections and queries

--- main.go
+++ main.go
@@ -7,29 +7,50 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"hbday/web-service-gin/models"
-	"log"
 	"net/http"
 	"time"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "gusampaio"
+	password = "gusampaio_pass"
+	dbname   = "hbday_db"
+)
+
 func main() {
-	// Initalize the sql.DB connection pool and assign it to the models.DB
-	// global variable.
-	var err error
-
-	models.DB, err = sql.Open("postgres", "postgres://gusampaio:gusampaio_pass@localhost/hbday_db")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("table company is created")
+	connectDb()
+	// close database
+	defer models.DB.Close()
 
 	router := gin.Default()
 	router.GET("/hello/:username", getPersonByUsername)
 	router.GET("/hello/all", getAll)
 	router.PUT("/hello/:username", postPerson)
-	router.Run("localhost:8080")
+	router.Run("localhost:8080") //nolint:errcheck
 }
+ func connectDb() {
+	 var err error
+	 // connection string
+	 psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	 // open database
+	 models.DB, err = sql.Open("postgres", psqlconn)
+	 if err != nil {
+		 panic(err)
+	 }
+
+	 // check db
+	 err = models.DB.Ping()
+	 if err != nil {
+		 panic(err)
+	 }
+	 fmt.Println("Connected!")
+	 // creating db table if doesn't exist
+	 models.CreateTable()
+ }
+
 
 func getAll(c *gin.Context) {
 	all, err := models.GetAllPeople()
@@ -42,17 +63,19 @@ func getAll(c *gin.Context) {
 // getPersonByUsername locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
 func getPersonByUsername(c *gin.Context) {
-	//username := c.Param("username")
+	username := c.Param("username")
+	if !exist(username){
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "username not found"})
+		return
+	}
 
-	// Loop through the list of people, looking for
-	// an person whose username value matches the parameter.
-/*	for _, a := range people {
-		if a.Username == username {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}*/
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "username not found"})
+	newP, err := models.GetPerson(username)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, newP)
 }
 
 // postPerson adds a person from JSON received in the request body.
@@ -65,21 +88,34 @@ func postPerson(c *gin.Context) {
 		return
 	}
 
+	// Including username param as not being passed in the JSON
+	newPerson.Username = c.Param("username")
+
+	if exist(newPerson.Username) {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "User already exist"})
+		return
+	}
+
 	err := isValidDoB(newPerson.DateOfBirth)
 	if err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
-	// Including username param as not being passed in the JSON
-	newPerson.Username = c.Param("username")
 
 	err = models.SetNewPerson(newPerson)
 	if err != nil {
-		panic(err)
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 	}
+
 	// Add the new person to the slice.
 	c.IndentedJSON(http.StatusNoContent, newPerson)
 }
+
+func exist(username string) bool{
+	newP, _ := models.GetPerson(username)
+	return len(newP.Username) != 0
+}
+
 
 func isValidDoB(dateOfBirthday string) error {
 	layout := "2006-01-02"
--- models/models.go
+++ models/models.go
@@ -11,10 +11,33 @@ type Person struct {
 	Username 	string `json:"username" gorm:"primaryKey"`
 	DateOfBirth string `json:"date_of_birth"` // YYYY-MM-DD
 }
+func CreateTable() error{
+	_, err := DB.Query("CREATE TABLE IF NOT EXISTS \"people\" (\n    username TEXT PRIMARY KEY,\n    date_of_birth TEXT NOT NULL\n);")
+	return err
+}
 
 func SetNewPerson(person Person) error{
-	_, err := DB.Query("INSERT INTO people (username, date_of_birth) VALUES (%s, %s);", person.Username, person.DateOfBirth)
+	dbQuery := "INSERT INTO people (username, date_of_birth) VALUES ($1, $2);"
+	_, err := DB.Exec(dbQuery, person.Username, person.DateOfBirth)
 	return err
+}
+
+func GetPerson(username string) (Person, error){
+	var person Person
+
+	rows, err := DB.Query("SELECT * FROM people WHERE username = $1", username)
+	if err != nil {
+		return  person, err
+	}
+
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		if err := rows.Scan(&person.Username, &person.DateOfBirth); err != nil {
+			return person, err
+		}
+	}
+	return person, nil
 }
 
 // AllBooks returns a slice of all books in the books table.
